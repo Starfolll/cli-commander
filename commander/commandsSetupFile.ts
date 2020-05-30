d5e69c9edfeee18815dbcfe677cb56c7e65b14a2
Starfolll <andrey.kovyarov@hotmail.com>
2020-05-30 15:16:16 +0300

    js -> yml

--- commander/commandsSetupFile.ts
+++ commander/commandsSetupFile.ts
@@ -1,7 +1,14 @@
-const fileData = (projectName: string) => `module.exports = {
-   name: "${!!projectName ? projectName : "myProject"}",
-   header: " This files contains commands and their description. \\n Editing is available by changing commands.js file. \\n To enter section you need to print its name. \\n To run command write: -<<commandName>>",
-   currentDirPath: process.cwd()
-};`;
+export default function getYamlSetupFile(projectName: string): string {
+   const defaultFileHeader = ' This files contains commands and their description. Editing is available by changing commands.js file. To enter section you need to print its name. To run command write: -<<commandName>>';
+   const currentDirPath = process.cwd();
 
-export default fileData;
+   const defaultFileObject: { [key: string]: any } = {
+      name: !!projectName ? projectName : "myProject",
+      header: `"${defaultFileHeader}"`,
+      currentDirPath: currentDirPath
+   };
+
+   return Object.keys(defaultFileObject).reduce((p, c) => {
+      return `${p}${c}: ${defaultFileObject[c]}\n`;
+   }, "");
+};
